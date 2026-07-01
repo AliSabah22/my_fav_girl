d7a2e4e8824780dd071c863b11e5de413a19fba4
AliSabah22 <aliasabah11@gmail.com>
2026-06-30 20:25:15 -0400

    Add stage and photo interlude content

---
+++ app/data/photos.ts
@@ -0,0 +1,15 @@
+import type { PhotoInterludeData } from "../types";
+
+export const photoInterlude: PhotoInterludeData = {
+  windowStart: 90,
+  windowEnd: 115,
+  photos: [
+    { src: "/photos/01.jpg", caption: "" },
+    { src: "/photos/02.jpg", caption: "" },
+    { src: "/photos/03.jpg", caption: "" },
+    { src: "/photos/04.jpg", caption: "" },
+    { src: "/photos/05.jpg", caption: "" },
+    { src: "/photos/06.jpg", caption: "" },
+    { src: "/photos/07.jpg", caption: "" },
+  ],
+};
